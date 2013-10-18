c644d637a5003c43182e7f2f324d9041585ed16e
Llanos Tobarra <llanos@scc.uned.es>
2013-10-18 16:51:24 +0200

    Web panel

--- sna.py
+++ sna.py
@@ -7,7 +7,7 @@ import matplotlib.pyplot as plt
 import json, io,db
 import networkx as nx
 from networkx.readwrite import json_graph
-
+from datetime import *
 #############################################################################################################
 def social_network():
     foros=db.mySQLConect("localhost","root","pfc2011","rita")
@@ -15,15 +15,22 @@ def social_network():
     edges=[]
 
     for idx,m in msg[msg.referedTo.notnull()].iterrows():
-        sender=long(m['sender'])
-        receiver=long(msg[msg.idMessage==m['referedTo']]['sender'])
-        edges.append((sender,receiver))
+        if(m['date']<datetime.strptime('08/01/11', "%m/%d/%y").date()): 
+            course='2010-2011'
+        elif(m['date']<datetime.strptime('08/01/12', "%m/%d/%y").date()): 
+            course='2011-2012'
+            sender=long(m['sender'])
+            receiver=long(msg[msg.idMessage==m['referedTo']]['sender'])
+            edges.append((sender,receiver))
+        else:
+            course='2012-2013'
+    wedges=[(x,y,{"weight": edges.count((x,y))}) for (x,y) in edges]
     graph=nx.DiGraph()
-    for (x,y) in edges:  
-        graph.add_edge(x,y,weight=edges.count((x,y)))
+    graph.add_edges_from(wedges)
         
     data=dict(nodes=graph.nodes(), edges=graph.edges())
-    with io.open('C:/Users/Llanos/Documents/GitHub/AutoESDashboard/html/network.json', 'w', encoding='utf-8') as f:
+    #data=json_graph.node_link_data(graph)
+    with io.open('C:/Users/Llanos/Documents/GitHub/AutoESDashboard/html/data/network.json', 'w', encoding='utf-8') as f:
         f.write(unicode(json.dumps(data, ensure_ascii=False)))
 #############################################################################################################
 
